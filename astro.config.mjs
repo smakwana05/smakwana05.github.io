862c9178be836d6e10c0de8e384ceb6c0b11905c
surajmakwana <smakwana05@users.noreply.github.com>
2024-01-08 00:21:11 +0000

    hello

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
-import swup from '@swup/astro';
+// import swup from '@swup/astro';
 import preact from "@astrojs/preact";
+// import Swup from 'swup';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     preact(), 
-    swup({
-      globalInstance: true,
-      animateHistoryBrowsing: true,
-    }),
+    // swup({
+    //   globalInstance: true,
+    //   animateHistoryBrowsing: true,
+    //   native: true,
+    //   skipPopStateHandling: (event) => event.state?.source !== 'swup'
+    // }),
   ]
 });
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,43 @@
+import Swup from 'swup';
+import SwupHeadPlugin from '@swup/head-plugin';
+import SwupScriptsPlugin from '@swup/scripts-plugin';
+import SwupProgressPlugin from '@swup/progress-plugin';
+import SwupPreloadPlugin from '@swup/preload-plugin';
+const header = document.querySelector('header');
+var navlinks = document.querySelectorAll('a');
+
+
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
 });
+
+addEventListener("popstate", (event) => {
+    console.log("popostateevent");
+    console.log(event.state?.source);
+   });
+   
+
+    const swup = new Swup({
+      animateHistoryBrowsing: true,
+      containers: ["#swup"],
+      plugins: [
+        new SwupHeadPlugin(),
+        new SwupScriptsPlugin(),
+        new SwupPreloadPlugin(),
+      ]
+    });
+
+swup.hooks.on('visit:start', (visit) => {
+    console.log('New page loaded:', visit.to.url);
+    // navlinks.blur();
+    
+
+    if (visit.to.url.includes("about")) {
+        console.log("aboutpage");
+        header.classList.add('bigheader');
+    }
+    else {header.classList.remove('bigheader');}
+},
+{priority:-100}
+);
 
