037c1c10b73eddca2954e05043c62b56d77f4531
surajmakwana <smakwana05@users.noreply.github.com>
2024-01-09 17:30:43 +0000

    3

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,36 +1,119 @@
 import Swup from 'swup';
 import SwupHeadPlugin from '@swup/head-plugin';
 import SwupScriptsPlugin from '@swup/scripts-plugin';
-import SwupProgressPlugin from '@swup/progress-plugin';
 import SwupPreloadPlugin from '@swup/preload-plugin';
+import SwupA11yPlugin from '@swup/a11y-plugin';
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import imagesLoaded from 'imagesloaded';
+
+
 const header = document.querySelector('header');
-var navlinks = document.querySelectorAll('a');
+const fb = Fancybox.getInstance();
 
 
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
+const swup = new Swup({
+  animateHistoryBrowsing: true,
+  containers: ["#swup"],
+  plugins: [
+    new SwupHeadPlugin(),
+    new SwupScriptsPlugin(),
+    new SwupPreloadPlugin(),
+    new SwupA11yPlugin(),
+  ]
 });
 
-addEventListener("popstate", (event) => {
-    console.log("popostateevent");
-    console.log(event.state?.source);
-   });
-   
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('expanded');
+});
 
-    const swup = new Swup({
-      animateHistoryBrowsing: true,
-      containers: ["#swup"],
-      plugins: [
-        new SwupHeadPlugin(),
-        new SwupScriptsPlugin(),
-        new SwupPreloadPlugin(),
-      ]
+
+
+function fancyboxOpening(visit) {
+    if(visit.to.url.includes('blog')) {
+      Fancybox.close();
+    }
+
+    if(visit.from.url.includes('blog') && visit.history.direction === 'forwards') {
+      const lastslide = localStorage.getItem("closedFBindex");
+      
+      Fancybox.fromSelector('[data-fancybox]', {
+        startIndex: lastslide,
+      });
+    }
+
+
+    if(document.querySelectorAll("[data-fancybox]") !== null) {
+    
+      let timer;
+        window.addEventListener('popstate', function() {
+          clearTimeout(timer); // cancel the history.back() if a popstate event occurs
+        });
+
+      Fancybox.bind("[data-fancybox]", {
+        Hash: false,
+        
+        on: {
+          "init": (fb) => { 
+              console.log("init");
+          },
+
+          "close": (fb) => {
+              console.log("close");  
+              const indexonclose = fb.getSlide().index;
+              localStorage.setItem("closedFBindex", indexonclose);
+              timer = setTimeout(function() {
+                history.back(); // go back after a delay if no popstate event has occurred
+              }, 25); 
+          },
+        },
+
+      });
+  }
+}
+
+
+document.addEventListener('swup:visit:start', ({ detail: { visit } }) => {
+  // console.log('Going to', visit.to.url);
+  fancyboxOpening(visit);
+  
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  if(document.querySelectorAll("[data-fancybox]") !== null) {
+    
+    let timer;
+      window.addEventListener('popstate', function() {
+        clearTimeout(timer); // cancel the history.back() if a popstate event occurs
+      });
+
+    Fancybox.bind("[data-fancybox]", {
+      Hash: false,
+      
+      on: {
+        "init": (fb) => { 
+            console.log("init");
+        },
+
+        "close": (fb) => {
+            console.log("close");  
+            const indexonclose = fb.getSlide().index;
+            localStorage.setItem("closedFBindex", indexonclose);
+            timer = setTimeout(function() {
+              history.back(); // go back after a delay if no popstate event has occurred
+            }, 25); 
+        },
+      },
+
     });
+}
+});
+
 
 swup.hooks.on('visit:start', (visit) => {
     console.log('New page loaded:', visit.to.url);
     // navlinks.blur();
-    
 
     if (visit.to.url.includes("about")) {
         console.log("aboutpage");
@@ -40,4 +123,12 @@ swup.hooks.on('visit:start', (visit) => {
 },
 {priority:-100}
 );
+
+
+swup.hooks.on('visit:start', (visit) => {
+  if (visit.history.popstate) {
+    console.log('popstate, direction=', visit.history.direction);
+  }
+
+}); 
 
