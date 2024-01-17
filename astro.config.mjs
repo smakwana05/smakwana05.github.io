f795804147548a42d8d7be9e2f1bd0f3503924d0
surajmakwana <smakwana05@users.noreply.github.com>
2024-01-17 17:57:52 +0000

    test workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,17 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()
+  site: 'https://smakwana05.github.io',
+  base: '/Astro',
+
+  integrations: [
+    // preact()
   // swup({
   //   globalInstance: true,
   //   animateHistoryBrowsing: true,
   //   native: true,
   //   skipPopStateHandling: (event) => event.state?.source !== 'swup'
   // }),
-  , react()]
+  // , react()
+]
 });
