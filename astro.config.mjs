af7ae46a2922e12b495faf9aa7474a7e236712db
surajmakwana <smakwana05@users.noreply.github.com>
2024-01-17 18:24:21 +0000

    base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
-// import swup from '@swup/astro';
-import preact from "@astrojs/preact";
-// import Swup from 'swup';
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://smakwana05.github.io',
-  base: '/',
+  base: '',
 
   integrations: [
     // preact()
