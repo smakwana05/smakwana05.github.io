f0a38c1db15e13a9f4cd3494c5a92e3ff9c281a2
surajmakwana <smakwana05@users.noreply.github.com>
2024-01-18 18:08:17 +0000

    Update

--- src/scripts/Swup.js
+++ src/scripts/Swup.js
@@ -3,6 +3,7 @@ import SwupHeadPlugin from '@swup/head-plugin';
 import SwupScriptsPlugin from '@swup/scripts-plugin';
 import SwupPreloadPlugin from '@swup/preload-plugin';
 import SwupA11yPlugin from '@swup/a11y-plugin';
+import SwupDebugPlugin from '@swup/debug-plugin';
 import imagesLoaded from 'imagesloaded';
 import { gsap } from "gsap";
 
@@ -23,10 +24,12 @@ const swup = new Swup({
       new SwupScriptsPlugin(),
       new SwupPreloadPlugin(),
       new SwupA11yPlugin(),
+      new SwupDebugPlugin(),
     ]
   });
 
 // gsap.to(preloader, {autoAlpha: 1, duration: 0.25, delay: 0.35 });   
+// gsap.set(preloader, {autoAlpha: 1});
 
 document.addEventListener('DOMContentLoaded', () => {
   gsap.to(preloader, {autoAlpha: 0});
@@ -48,12 +51,27 @@ swup.hooks.on('visit:start', (visit) => {
 
 let preloaderTimeout;
 
-swup.hooks.on('animation:out:start', () => {
-  // {gsap.to(preloader, {autoAlpha: 1, duration: 0.25 }) }  
-  preloaderTimeout = setTimeout(() => {
-    gsap.to(preloader, {autoAlpha: 1, duration: 0.25 });
-  }, 300);
+document.addEventListener('swup:animation:out:start', ({ detail: { visit } }) => {
+  // setTimeout(() => {
+    gsap.to(preloader, {autoAlpha: 1});
+  // }, 20);
 });
+
+document.addEventListener('swup:animation:in:end', ({ detail: { visit } }) => {
+  imagesLoaded(allcontent, function (instance) {
+    // preloader.classList.add('hidden')
+    gsap.to(preloader,{autoAlpha: 0});
+  });
+});
+
+
+
+// swup.hooks.before('animation:out:start', () => {
+//   // {gsap.to(preloader, {autoAlpha: 1, duration: 0.25 }) }  
+//   preloaderTimeout = setTimeout(() => {
+//     gsap.to(preloader, {autoAlpha: 1, duration: 0.25});
+//   }, 260);
+// });
 
 swup.hooks.replace('animation:out:await', async () => {
    await gsap.to('.gridwrapper', { autoAlpha: 0, duration: 0.25 });
@@ -64,10 +82,11 @@ swup.hooks.replace('animation:in:await', async () => {
 
   await imagesLoaded(document.querySelector('.gridwrapper'), function(instance) {
     // console.log('imageslode');
-    clearTimeout(preloaderTimeout);
+    // clearTimeout(preloaderTimeout);
 
-    gsap.to(preloader, {autoAlpha: 0});
-    gsap.to('.gridwrapper', { autoAlpha: 1, duration: 0.5 });
+    // gsap.to(preloader, {autoAlpha: 0});
+
+    gsap.to('.gridwrapper', { autoAlpha: 1 });
   });
 },{priority: -100});
 
